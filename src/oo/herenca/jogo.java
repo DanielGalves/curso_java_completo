3cfc1b729f8fe9681cd516af0b955206f50598bd
Daniel <danielgama1@live.com>
2023-07-06 20:47:12 -0300

    Inserido novas classes da aula de herança onde as classes Heroi e Monstro herdaram os atributos da Classe mãe Jogador

--- src/oo/herenca/jogo.java
+++ src/oo/herenca/jogo.java
@@ -4,17 +4,29 @@ public class jogo {
 	
 	public static void main(String args[]) {
 		
-		Jogador j1 = new Jogador();
+		Heroi j1 = new Heroi();
 		j1.x = 20;
 		j1.y = 10;
 		
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.SUL);
-		j1.andar(Direcao.OESTE);
+		Monstro j2 = new Monstro();
+		j2.x = 20;
+		j2.y = 11;
 		
-		System.out.println(j1.y);
-		System.out.println(j1.x);
+		System.out.println("Vida do Heroi:   " + j1.vida);
+		System.out.println("Vida do Monstro: " + j2.vida);
+		
+		
+		j1.atacar(j2);
+		j2.atacar(j1);
+		
+		System.out.println("Vida do Heroi:   " + j1.vida);
+		System.out.println("Vida do Monstro: " + j2.vida);
+		
+		
+		//j1.andar(Direcao.OESTE);
+		
+		//System.out.println(j1.y);
+		//System.out.println(j1.x);
 		
 		
 		
