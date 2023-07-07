29ca5625accaeac5bcbce4d5408393f2f8959aa4
Daniel <danielgama1@live.com>
2023-07-06 21:21:03 -0300

    Realizado a subscrição do metodo atacar na classe Heroi e na classe Mostro de formas diferente porem com o mesmo resultado

--- src/oo/herenca/Heroi.java
+++ src/oo/herenca/Heroi.java
@@ -1,5 +1,13 @@
 package oo.herenca;
 
 public class Heroi extends Jogador{
+	
+	boolean atacar(Jogador oponente) {
+		//cria uma variavel de nome ataque que recebe o metodo atacar da classe pai que foi chamada utilizando o metodo super
+		boolean ataque1 = super.atacar(oponente);
+		boolean ataque2 = super.atacar(oponente);
+		//retorna o conteudo da variavel ataque
+		return ataque1 || ataque2 ;
+	}
 
 }
--- src/oo/herenca/jogo.java
+++ src/oo/herenca/jogo.java
@@ -4,23 +4,23 @@ public class jogo {
 	
 	public static void main(String args[]) {
 		
-		Heroi j1 = new Heroi();
-		j1.x = 20;
-		j1.y = 10;
+		Heroi heroi = new Heroi();
+		heroi.x = 20;
+		heroi.y = 10;
 		
-		Monstro j2 = new Monstro();
-		j2.x = 20;
-		j2.y = 11;
+		Monstro monstro = new Monstro();
+		monstro.x = 20;
+		monstro.y = 11;
 		
-		System.out.println("Vida do Heroi:   " + j1.vida);
-		System.out.println("Vida do Monstro: " + j2.vida);
+		System.out.println("Vida do Heroi:   " + heroi.vida);
+		System.out.println("Vida do Monstro: " + monstro.vida);
 		
 		
-		j1.atacar(j2);
-		j2.atacar(j1);
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
 		
-		System.out.println("Vida do Heroi:   " + j1.vida);
-		System.out.println("Vida do Monstro: " + j2.vida);
+		System.out.println("Vida do Heroi:   " + heroi.vida);
+		System.out.println("Vida do Monstro: " + monstro.vida);
 		
 		
 		//j1.andar(Direcao.OESTE);
