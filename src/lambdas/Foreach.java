e9e86190ab6322a2297a404160a1a7a056d9c386
Daniel <danielgama1@live.com>
2023-08-06 22:35:54 -0300

    Inserido nofa forma de executar a mesma ação que um forEach tradicional, utilizando uma funçao lambda e agora um method reference

--- src/lambdas/Foreach.java
+++ src/lambdas/Foreach.java
@@ -15,10 +15,12 @@ public class Foreach {
 	
 		System.out.println("\nLambda #01...");
 		aprovados.forEach((nome) -> {System.out.println(nome + "!!!");});
-		
 		//forma simplificada do codigo anterior
 		System.out.println("\nLambda #02...");
 		aprovados.forEach(nome -> System.out.println(nome + "!!!"));
+		
+		System.out.println("\nMethod Reference...");
+		aprovados.forEach(System.out::println);
 	
 	}
 }
